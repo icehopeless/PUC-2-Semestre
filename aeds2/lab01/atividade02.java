5397da1331806e16c32a0fe5ec5ef6f69bad9503
Gabriel Agostinho <gabrielagostinhodasilva@hotmail.com>
2026-03-09 11:50:34 -0300

    atividade02 lab01

--- aeds2/lab01/atividade02.java
+++ aeds2/lab01/atividade02.java
@@ -3,14 +3,33 @@ import java.util.Vector;
 
 public class atividade02{
 	public static int countMaiusculas(String str, int i){
-		if(str.charAt() >= 65 && str.charAt(i) <=90){
-			return 1;
+		int count = 0;
+		if(str.charAt(i) >= 65 && str.charAt(i) <=90){
+			count = 1;
 		}
-		if(!str.length() <= 1){
+		if(i <= 1){
+			count = 0;
 			return 0;	
 		}
+		else{
+			return count + countMaiusculas(str, i-1);
+		}
+
 
 	}
 
+	public static void main (String[] args){
+		Vector<String> lines = new Vector<String>();
+		Scanner scan = new Scanner (System.in);
 
+		do{
+			lines.add(scan.nextLine());
+		}while(!lines.lastElement().equals("FIM"));
+
+		for(int i =0; i < lines.size(); i++){			
+			System.out.println(countMaiusculas(lines.elementAt(i), lines.elementAt(i).length()-1 ));
+		}
+	}
+
+	
 }
