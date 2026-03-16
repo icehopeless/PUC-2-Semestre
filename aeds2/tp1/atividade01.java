46e21b97c84e8103d8d96c24738b5ac1b366acba
Gabriel Agostinho <gabrielagostinhodasilva@hotmail.com>
2026-03-16 18:26:07 -0300

    questao01 tp1

--- aeds2/tp1/atividade01.java
+++ aeds2/tp1/atividade01.java
@@ -2,21 +2,22 @@ import java.util.*;
 
 
 public class atividade01{
-	public int key = 3;
+	public static int key = 3;
 
 	//funcao principal para fazer o ciframento
 	public static String cifraCesar(String str){
+		char[] chars = new char[str.length()];
 		for(int i = 0; i < str.length(); i++){
-			str[i] = str[i]+3;
+			chars[i] = (char)(str.charAt(i) + key);
 		}
 		
-		return str;
+		return new String(chars);
 	}
 
 	public static void main (String[] args){
 		String in;
 		int count = 0;
-		String[][] lines = new String [100][100];
+		String[] lines = new String [100];
 		Scanner scan = new Scanner(System.in);
 
 		while((scan.hasNextLine())){		
@@ -24,20 +25,16 @@ public class atividade01{
 			count++;
 		}
 
-		String[][] out = new String [100][count];
+		String[] out = new String[count];
 
 
-		for(int i = 0; i <= count; i++){
+		for(int i = 0; i < count; i++){
 			out[i] = cifraCesar(lines[i]); 
 		}
-
-		for (String[] linha : out) {
-   			for (String str : linha) {
-      				MyIO.println(linha);
-    			}
+		
+		for (String s : out) {
+			System.out.println(s);
 		}
-		
-		
 	}
 	
 }
