b75d7ca139f3e1844735b8abde5aa6b0ade6fcf6
Gabriel Agostinho <gabrielagostinhodasilva@hotmail.com>
2026-03-17 21:16:43 -0300

    Started atividade04

--- aeds2/tp1/atividade01.java
+++ aeds2/tp1/atividade01.java
@@ -4,6 +4,21 @@ import java.util.*;
 public class atividade01{
 	public static int key = 3;
 
+	//funcao para verificar se uma string e igual a outra
+	public static boolean compareStrings(String str1, String str2){
+		if(str1.length() != str2.length()){
+			return false;
+		}
+		else{
+			for(int i = 0; i < str1.length(); i++){
+				if(str1.charAt(i) != str2.charAt(i)){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
 	//funcao principal para fazer o ciframento
 	public static String cifraCesar(String str){
 		//e necessario utilizar um array de caracter literal, pois a String em java e imutavel, e nao podemos alterar um caracter especifico que desejamos.
@@ -20,8 +35,11 @@ public class atividade01{
 		String[] lines = new String [1000];
 		Scanner scan = new Scanner(System.in);
 
-		while(!(lines[count] = scan.nextLine()).equals("FIM")){		
+		String linha = scan.nextLine();
+		while(!compareStrings(linha, "FIM")){
+			lines[count] = linha;
 			count++;
+			linha = scan.nextLine();
 		}
 		
 		for(int i = 0; i < count; i++){
--- aeds2/tp1/atividade03.java
+++ aeds2/tp1/atividade03.java
@@ -1,8 +1,24 @@
 import java.util.*;
 
 public class atividade03 {
-    static char[] vogais = {a, e, i, o, u};
+    static char[] vogais = {'a', 'e', 'i', 'o', 'u'};
 
+    //funcao para verificar se uma string e igual a outra
+    public static boolean compareStrings(String str1, String str2){
+		if(str1.length() != str2.length()){
+			return false;
+		}
+		else{
+			for(int i = 0; i < str1.length(); i++){
+				if(str1.charAt(i) != str2.charAt(i)){
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+    //recebe um caracter e retorna true caso o caracter seja uma vogal
     public static boolean compareCasesVogal(char chr){
         boolean aux = false;
         for(char v : vogais){
@@ -15,12 +31,102 @@ public class atividade03 {
         return aux;
     }
     
-    public boolean fullVogal(String str){
+    //metodo para verificar se a string e composta apenas por vogais
+    public static boolean fullVogal(String str){
         for(int i = 0; i < str.length(); i++){
-            if(!compareCases(str.charAt(i))){
+            if(!compareCasesVogal(str.charAt(i))){
                 return false;
             }
         }
         return true;
     }
+
+    //metodo para verificar se a string e composta somente por consoantes
+    public static boolean fullConsoante(String str){
+        for(int i = 0; i < str.length(); i++){
+            char c = str.charAt(i);
+
+            if(!Character.isLetter(c) || compareCasesVogal(c)){
+                return false;
+            }
+        }
+    return true;
+}
+
+    //metodo para verificar se a string e um inteiro
+    public static boolean isInteiro(String str){
+        for(int i = 0; i < str.length(); i++){
+            if(!(str.charAt(i) >= '0' && str.charAt(i) <= '9')){
+                return false;
+            }
+            if(str.charAt(i) == '.' || str.charAt(i) == ','){
+                return false;
+            }
+        }
+        return true;
+    }
+
+//metodo para verificar se a string e um numero real
+    public static boolean isReal(String str){
+        boolean aux = false;
+        int points = 0;
+        for(int i = 0; i < str.length(); i++){
+            if(str.charAt(i) == '.' || str.charAt(i) == ','){
+                points++;
+                aux = true;
+                if(points>1){
+                    return false;
+                }
+            }
+            else if(!(str.charAt(i) >= '0' && str.charAt(i) <= '9')){
+                return false;
+            }
+        }
+        return aux;
+    }
+
+    public static void main(String[] args) {
+        int count = 0;
+		String[] lines = new String [1000];
+		Scanner scan = new Scanner(System.in);
+        String out = new String();
+
+		String linha = scan.nextLine();
+		while(!compareStrings(linha, "FIM")){
+			lines[count] = linha;
+			count++;
+			linha = scan.nextLine();
+		}
+
+        
+		for(int i = 0; i < count; i++){
+    		if(fullVogal(lines[i])){
+                out += "SIM";
+            }else{
+                out += "NAO";
+            }
+
+            if(fullConsoante(lines[i])){
+                out += " SIM";
+            }else{
+                out += " NAO";
+            }
+
+            if(isInteiro(lines[i])){
+                out += " SIM";
+            }else{
+                out += " NAO";
+            }
+
+            if(isReal(lines[i])){
+                out += " SIM";
+            }else{
+                out += " NAO";
+            }
+
+            System.out.println(out);
+            out = "";
+		}
+	} 
+
 }
